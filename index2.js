a739f03335c01c1e0a7b55bd3f39b8d3d480d529
Hala-Almasri <s12325743@stu.najah.edu>
2025-11-19 21:37:25 +0200

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,15 +1,16 @@
-age(40,100)
-function age(x) {
-let year = 2025-x;
-console.log(year);
-if (x>70){
-    console.log("You are old")
-}
-else{
-    console.log("you are young")
+let x=60 ;
+if (x<=50){
+    console.log("fail")
 
 }
+   
+
+else {
+    console.log("pass")
+
 }
+
+
 
 
 
