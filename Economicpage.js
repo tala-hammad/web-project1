96832b679d5c751f0d6467a0e0272f0130b7a1a7
Rateeba-Dawahy <s12325539@stu.najah.edu>
2025-12-09 17:35:26 -0800

    the last changes

--- Economicpage.js
+++ Economicpage.js
@@ -1,7 +1,7 @@
-const Economicpage=window.location.pathname.split('/').pop();
+const EconomicpageURL=window.location.pathname.split('/').pop();
 const buttonlinks=document.querySelectorAll('.inline');
 buttonlinks.forEach(link => {
-    if (link.getAttribute('href') === Economicpage ) {
+    if (link.getAttribute('href') === EconomicpageURL ) {
         link.classList.add('active-btn');
     }
 });
