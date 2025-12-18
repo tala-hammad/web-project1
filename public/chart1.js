3563b5ed568fd3d8eb94bebd6bfe50aca4b01de0
s12325743@stu.najah.edu <s12325743@stu.najah.edu>
2025-12-18 04:17:30 +0200

    last changes

---
+++ public/chart1.js
@@ -0,0 +1,40 @@
+ const ctx = document.getElementById('barchart').getContext('2d');
+
+   
+    const barchart = new Chart(ctx, {
+      type: 'bar', 
+      data: {
+        
+        labels: ['18-25', '26-35', '36-45', '46-55', '56-65', '65+'],
+        datasets: [{
+          label: 'Mental Wellbeing by Age Group',
+          data: [12, 19, 15, 25, 5, 7], 
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(192, 199, 67, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1 
+        }]
+      },
+      options: {
+       responsive:true,
+   
+        scales: {
+          y: {
+            beginAtZero: true 
+          }
+        }
+      }
+    });
