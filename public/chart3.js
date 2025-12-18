c1e8f11470656fb91e88d072b94a0f48b743aa83
s12325743@stu.najah.edu <s12325743@stu.najah.edu>
2025-12-18 16:37:00 +0200

    last changes

--- public/chart3.js
+++ public/chart3.js
@@ -14,9 +14,72 @@ var chartOptions={
     },
     series:[
           {
-            name:'music'
-        }
-    ]
+            name:'music',
+            data:[3,33,21,42,19,32],
+        },
+        {
+            name:'photos',
+            data:[3,33,21,42,19,32],
+
+        },
+        {
+            name:'Files',
+            data:[0,39,52,11,29,43],
+        },
+        
+        titel:{
+            text:'Media',
+            align :'left',
+            offsetY:25,
+            offsetX:5,
+            style:{
+              fontSize:'14px',
+              fontWeight :'bold', 
+              color:'#373d3f' 
+            },
+        },
+        markers:{
+            size:6,
+            strokeWidth:0,
+            hover:{
+                size:9,
+
+            },
+        },
+        
+grid:{
+    show:true,
+    padding:{
+        bottom:0,
+    },
+},
+labels:['2018','2019','2020','2021','2022','2023'],
+xaxis:{
+    tooltip:{
+        enabled:false,
+    },
+},
+legend:{
+position:'top',
+horizontalAlign:'right',
+offsetY:-10,
+labels:{
+    colors:'#373d3f',
+
+},
+
+},
+grid:{
+    borderColor:'#D9D8F3'
+    xaxis:{
+        lines:{
+            show:true,
+        },
+    },
+},
+
+        };
+
+    
       
-    }
-}
+    
