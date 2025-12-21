9c78e832238df11389e70930be4710bcf23c7826
s12325743@stu.najah.edu <s12325743@stu.najah.edu>
2025-12-21 03:19:39 +0200

    last changes

--- public/chart1.js
+++ public/chart1.js
@@ -2,17 +2,17 @@ const ctx = document.getElementById('barchart').getContext('2d');
 const barchart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['18-25', '26-35', '36-45', '46-55', '56-65', '65+'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Mental Wellbeing by Age Group',
+            data: [12.0, 14.2, 15.5, 16.8, 18.3,17.5],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(153, 102, 255, 0.5)',
+                'rgba(255, 159, 64, 0.5)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
@@ -26,9 +26,32 @@ const barchart = new Chart(ctx, {
         }]
     },
     options: {
+        responsive:true,
+        maintainAspectRatio:false,
         scales: {
             y: {
-                beginAtZero: true
+                beginAtZero: true,
+                max:20,
+                title:{
+                    display:true,
+                    text:'Wellbeing Score (Higher is Better)',
+                    font:{size:14,weight:'bold'}
+                }
+
+            },
+            x:{
+                title:{
+                    display:true,
+                    text:'Age Groups (Years)',
+                    font:{size:14,weight:'bold'}
+                }
+            }
+        },
+        plugins:{
+            title:{
+                display:true,
+                text:'Mental Wellbeing by Age Group in Palestine',
+                font:{size:18}
             }
         }
     }
--- public/chart2.js
+++ public/chart2.js
@@ -5,34 +5,37 @@
       type: 'doughnut', 
       data: {
         
-        labels: ['18-25', '26-35', '36-45', '46-55', '56-65', '65+'],
+        labels: ['Direct Violence Exposure', 'Media Coverage Witnessing','Children & Youth (Nightmares)' ],
         datasets: [{
-          label: 'Mental Wellbeing by Age Group',
-          data: [12, 19, 10, 25, 8, 4], 
+          label: 'Psychological Impact Percentage',
+          data: [50, 80, 60], 
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
+            'rgba(255, 99, 132, 0.6)',
+            'rgba(54, 162, 235, 0.6)',
+            'rgba(255, 206, 86, 0.6)',
           ],
           borderColor: [
             'rgba(255, 99, 132, 1)',
             'rgba(54, 162, 235, 1)',
-            'rgba(192, 199, 67, 1)',
             'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
           ],
           borderWidth: 1 
         }]
       },
       options: {
-        scales: {
-          y: {
-            beginAtZero: true 
+        responsive:true,
+        plugins:{
+          legend:{
+            position:'bottom',
+          },
+          title:{
+            display:true,
+            text: 'Psychological Impact by Exposure Type (Nablus)',
+            font:{size:18}
+
           }
         }
       }
+        
+      
     });
